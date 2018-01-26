b5809acb0c9a2382c0ee9d88f3d73b0f37373f01
Jacob Busfield <jacob.busfield@hotmail.co.uk>
2018-01-26 21:14:50 +0000

    Screen size change

--- JS/HospitalBedlam.js
+++ JS/HospitalBedlam.js
@@ -1,6 +1,6 @@
 var game;
 
-game = new Phaser.Game(1024, 520, Phaser.AUTO, 'phaserGame');
+game = new Phaser.Game(1380, 680, Phaser.AUTO, 'phaserGame');
 
 game.state.add('PreBoot', PreBoot);
 game.state.add('PreLoad', PreLoad);
