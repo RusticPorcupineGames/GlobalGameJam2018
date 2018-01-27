8a79ac36d28a67b0262d7fa1336fbe41323715f8
declan-morris <declan-morris@users.noreply.github.com>
2018-01-27 23:00:16 +0000

    added a restart button to the fail screen

--- JS/Failscreen.js
+++ JS/Failscreen.js
@@ -4,6 +4,13 @@ var FailScreen = {
       var style = { font: "32px Arial", fill: "#ffffff", align: "center" };
       game.add.text(300, 200, "Your moves have run out!", style);
       game.add.text(300,250, "Plan your intial infection carefully to infect all the patients", style);
+      this.restartButton = game.add.button(game.world.centerX,game.world.centerY, 'imgRestartButton', function() { return this.restartLevel(game.levelSelected);}, this, 1,0);
+
     },
     update : function() {},
+
+    restartLevel: function (level) {
+        game.levelSelected = level;
+        this.state.start('Game');
+    },
 };
