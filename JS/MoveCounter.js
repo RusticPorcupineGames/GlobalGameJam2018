51b44a8d1e83ddc571c23fdf0fdbfdc1300849bf
Jacob Busfield <jacob.busfield@hotmail.co.uk>
2018-01-27 19:22:55 +0000

    Extend MoveCounter

--- JS/MoveCounter.js
+++ JS/MoveCounter.js
@@ -1,6 +1,15 @@
 var MoveCounter = function(game){
-  var _moves = 5; // inital moves = 5.
+  var _initial = 5;
+  var _moves = _initial;
 
   var title = game.add.text(1175, 50, 'Moves:', { fontSize: '16px', fill: '#000' });
   var scoreText = game.add.text(1175, 64, _moves, { fontSize: '90px', fill: '#000' });
+
+  this.useMove = function(){
+    return --_moves;
+  }
+
+  this.resetMoves = function(){
+    _moves = _initial;
+  }
 }
