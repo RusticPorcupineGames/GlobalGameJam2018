61e4669e2a5361013fc530f3169ae7a626954d54
declan-morris <declan-morris@users.noreply.github.com>
2018-01-27 23:12:48 +0000

    failscreen button aesthetics

--- JS/Failscreen.js
+++ JS/Failscreen.js
@@ -4,7 +4,8 @@ var FailScreen = {
       var style = { font: "32px Arial", fill: "#ffffff", align: "center" };
       game.add.text(300, 200, "Your moves have run out!", style);
       game.add.text(300,250, "Plan your intial infection carefully to infect all the patients", style);
-      this.restartButton = game.add.button(game.world.centerX,game.world.centerY, 'imgRestartButton', function() { return this.restartLevel(game.levelSelected);}, this, 1,0);
+      this.restartButton = game.add.button(game.world.centerX,game.world.centerY+30, 'imgRestartButton', function() { return this.restartLevel(game.levelSelected);}, this, 1,0);
+      this.restartButton.anchor.setTo(0.5,0.5);
 
     },
     update : function() {},
