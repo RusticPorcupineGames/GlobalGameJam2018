bf41ea416e36c9a4f6f1b1687dd2a3d6c81b2bf9
declan-morris <declan-morris@users.noreply.github.com>
2018-01-28 09:22:01 +0000

    added a reset function to killcounter

--- JS/KillCounter.js
+++ JS/KillCounter.js
@@ -3,6 +3,10 @@ var KillCounter = function(game){
   var killtitle = game.add.text(100, 50, 'Patients Killed:', { fontSize: '16px', fill: '#000' }).anchor.setTo(0.5,0);
   var countKills = game.add.text(100, 64, _kills, { fontSize: '90px', fill: '#000' }).anchor.setTo(0.5,0);
 
+  this.reset = function() {
+       countKills.text = 0;
+  }
+
   this.update = function(){
     countKills.text = 3;
   }
