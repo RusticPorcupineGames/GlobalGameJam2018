6db3e78caf98181e65da5019b34d449cf53410e1
Aiste <aiste.kiskyte@gmail.com>
2018-01-28 10:47:23 +0000

    fixed counters

--- JS/KillCounter.js
+++ JS/KillCounter.js
@@ -1,14 +1,15 @@
 var KillCounter = function(game){
   var _kills = 0;
-  var killtitle = game.add.text(100, 50, 'Patients Killed:', { fontSize: '16px', fill: '#000' }).anchor.setTo(0.5,0);
-  var countKills = game.add.text(100, 64, _kills, { fontSize: '90px', fill: '#000' }).anchor.setTo(0.5,0);
+  var killtitle = game.add.text(100, 50, 'Patients Infected:', { fontSize: '16px', fill: '#000' }).anchor.setTo(0.5,0);
+  var countText = game.add.text(100, 64, _kills, { fontSize: '90px', fill: '#000' });
+
 
   this.reset = function() {
-       countKills.text = 0;
+      countText.text = 0;
   }
 
   this.update = function(){
-
-    countKills.text = _kills;
+      _kills++;
+      countText.text = _kills;
   }
 }
