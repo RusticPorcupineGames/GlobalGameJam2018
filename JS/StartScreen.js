60dee6145d2fec3d40db31cbbc9027f9db738f3b
Jacob Busfield <jacob.busfield@hotmail.co.uk>
2018-01-28 10:56:17 +0000

    12 levels

--- JS/StartScreen.js
+++ JS/StartScreen.js
@@ -5,19 +5,22 @@ var StartScreen = {
 
     create: function () {
 
-      this.logo = game.add.sprite(game.world.centerX, 100, 'logo');
-      this.logo.anchor.setTo(0.5,0.5);
+    this.logo = game.add.sprite(game.world.centerX, 150, 'logo');
+    this.logo.anchor.setTo(0.5,0.5);
 
-    // var totalLevels = 2;
-    // for (var level = 1; level < (totalLevels+1); level++)
-    // {
-    //    var xWidthOffset = level * (game.world.width / (totalLevels+1));
-    //    var buttonName = 'imgLevel' + level + 'Button';
-    //    game.add.button(xWidthOffset, 250, buttonName, function(){return this.startGame(level);}, this, 1, 0).anchor.setTo(0.5,0);
-    //  };
+    game.add.button(game.world.centerX - 500, 300, 'imgLevel1Button', function(){ return this.startGame(1);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX - 300, 300, 'imgLevel2Button', function(){ return this.startGame(2);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX - 100, 300, 'imgLevel2Button', function(){ return this.startGame(3);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX + 100, 300, 'imgLevel2Button', function(){ return this.startGame(4);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX + 300, 300, 'imgLevel2Button', function(){ return this.startGame(5);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX + 500, 300, 'imgLevel2Button', function(){ return this.startGame(6);}, this, 1,0).anchor.setTo(0.5,0);
 
-     game.add.button(game.world.centerX - 200, 200, 'imgLevel1Button', function(){ return this.startGame(1);}, this, 1,0);
-     game.add.button(game.world.centerX - 50, 200, 'imgLevel2Button', function(){ return this.startGame(2);}, this, 1,0);
+    game.add.button(game.world.centerX - 500, 500, 'imgLevel2Button', function(){ return this.startGame(7);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX - 300, 500, 'imgLevel2Button', function(){ return this.startGame(8);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX - 100, 500, 'imgLevel2Button', function(){ return this.startGame(9);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX + 100, 500, 'imgLevel2Button', function(){ return this.startGame(10);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX + 300, 500, 'imgLevel2Button', function(){ return this.startGame(11);}, this, 1,0).anchor.setTo(0.5,0);
+    game.add.button(game.world.centerX + 500, 500, 'imgLevel2Button', function(){ return this.startGame(12);}, this, 1,0).anchor.setTo(0.5,0);
 
         //this.music = game.add.audio('menuScreenMusic');
         //this.music.play();
